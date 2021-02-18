e683b8952332f258bc02424d9eab90302eb5042f
Mike Truso <mike@agileorbit.com>
2021-02-18 10:32:39 -0600

    jest tests

---
+++ tests/unit/SeasonCard.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import SeasonCard from '@/components/SeasonCard'
+
+describe('SesonCard.vue', () => {
+  it('renders props when passed', () => {
+    const msg = 'Spring February 1, 3, 4'
+    const wrapper = shallowMount(SeasonCard, {
+      propsData: { seasonName: 'Spring', startDate: new Date(2020, 1, 1, 0, 0, 0) }
+    })
+    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
