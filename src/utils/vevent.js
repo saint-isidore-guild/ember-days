ead0ae249d29270fde960fc3ea222c91ab332604
jHaben <jrhaben@gmail.com>
2022-01-05 18:58:02 -0600

    custom class to make events for ics file

---
+++ src/utils/vevent.js
@@ -0,0 +1,13 @@
+export default class Vevent {
+
+    constructor(productId, title, description, start, end, uid) {
+
+       this.productId = productId;
+       this.title = title;
+       this.description = description;
+       this.start = start;
+       this.end = end;
+       this.uid = uid;
+
+    }
+}
